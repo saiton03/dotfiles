e00cedbddcd39471409fea3fa9c20a6182a5f057
saiton03 <saiton15603@gmail.com>
2021-08-21 18:25:01 +0900

    update lsp settings

--- .vimrc
+++ .vimrc
@@ -126,3 +126,7 @@ nmap <leader>l <Plug>(easymotion-overwin-line)
 set termguicolors
 let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+
+" lsp integration
+
+
--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ DOTPATH=~/dotfiles
 for f in .??*
 do
     [[ $f == ".git" ]] || [[ $f == ".DS_STORE" ]] || [[ $f == ".vscode" ]] && continue
+    if [[ $f == ".config" ]]; then
+        ln -snfv "$DOTPATH"/"$f"/* "$HOME"/"$f"/
+        continue
+    fi
     ln -snfv "$DOTPATH"/"$f" "$HOME"/"$f"
 done
 
